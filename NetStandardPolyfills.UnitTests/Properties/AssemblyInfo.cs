35c1f7f59600134b41b20c663583ae18cd19cd06
Steve Wilkes <steve@agileobjects.co.uk>
2017-05-07 14:32:31 +0100

    Updating to v0.2.1

--- NetStandardPolyfills.UnitTests/Properties/AssemblyInfo.cs
+++ NetStandardPolyfills.UnitTests/Properties/AssemblyInfo.cs
@@ -2,6 +2,6 @@
 
 [assembly: AssemblyCompany("AgileObjects Ltd")]
 [assembly: AssemblyProduct("AgileObjects.NetStandardPolyfills")]
-[assembly: AssemblyCopyright("Copyright © AgileObjects Ltd 2016")]
+[assembly: AssemblyCopyright("Copyright © AgileObjects Ltd 2017")]
 [assembly: AssemblyTitle("AgileObjects.AgileMapper.UnitTests.Polyfills")]
-[assembly: AssemblyDescription("AgileObjects.AgileMapper.UnitTests.Polyfills")]
+[assembly: AssemblyDescription("Type and Reflection polyfill extension methods for .NET Standard v1.0 and .NET v4.0.")]
--- NetStandardPolyfills/Properties/AssemblyInfo.cs
+++ NetStandardPolyfills/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Security;
 
 [assembly: AssemblyCompany("AgileObjects Ltd")]
 [assembly: AssemblyProduct("AgileObjects.NetStandardPolyfills")]
-[assembly: AssemblyCopyright("Copyright © AgileObjects Ltd 2016")]
+[assembly: AssemblyCopyright("Copyright © AgileObjects Ltd 2017")]
 [assembly: AssemblyTitle("AgileObjects.NetStandardPolyfills")]
-[assembly: AssemblyDescription("AgileObjects.NetStandardPolyfills")]
+[assembly: AssemblyDescription("Type and Reflection polyfill extension methods for .NET Standard v1.0 and .NET v4.0.")]
 
 [assembly: NeutralResourcesLanguage("en")]
 [assembly: CLSCompliant(true)]
