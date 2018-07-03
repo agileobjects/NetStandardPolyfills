0d510d5dc717d97d6ebcedc11b54d9e18eb0eb4d
Steve Wilkes <SteveWilkes@users.noreply.github.com>
2018-07-03 14:04:00 +0100

    Net35 support (#4)
    * Add support for net20 and net35 TFMs (#3)
    
    * Adding net35 target and unit tests

---
+++ NetStandardPolyfills.UnitTests.Net35/WhenRetrievingTypeConstructors.cs
@@ -0,0 +1,44 @@
+using NUnit.Framework;
+
+namespace AgileObjects.NetStandardPolyfills.UnitTests.Net35
+{
+    [TestFixture]
+    public class WhenRetrievingTypeConstructors : ConstructorTestsBase
+    {
+        [Test]
+        public override void ShouldFindPublicInstanceConstructors() => DoShouldFindPublicInstanceConstructors();
+
+        [Test]
+        public override void ShouldFindAnImplicitPublicInstanceConstructor() => DoShouldFindAnImplicitPublicInstanceConstructor();
+
+        [Test]
+        public override void ShouldFindAnExplicitPublicInstanceConstructor() =>
+            DoShouldFindAnExplicitPublicInstanceConstructor();
+
+        [Test]
+        public override void ShouldFindAPublicInstanceConstructorByParameterTypes() =>
+            DoShouldFindAPublicInstanceConstructorByParameterTypes();
+
+        [Test]
+        public override void ShouldExcludeAPublicInstanceConstructorByParameterTypes() =>
+            DoShouldExcludeAPublicInstanceConstructorByParameterTypes();
+
+        [Test]
+        public override void ShouldFindNonPublicInstanceConstructors() => DoShouldFindNonPublicInstanceConstructors();
+
+        [Test]
+        public override void ShouldFindANonPublicInstanceConstructor() => DoShouldFindANonPublicInstanceConstructor();
+
+        [Test]
+        public override void ShouldExcludeANonPublicInstanceConstructor() =>
+            DoShouldExcludeANonPublicInstanceConstructor();
+
+        [Test]
+        public override void ShouldFindANonPublicInstanceConstructorByParameterTypes() =>
+            DoShouldFindANonPublicInstanceConstructorByParameterTypes();
+
+        [Test]
+        public override void ShouldExcludeANonPublicInstanceConstructorByParameterTypes() =>
+            DoShouldExcludeANonPublicInstanceConstructorByParameterTypes();
+    }
+}
